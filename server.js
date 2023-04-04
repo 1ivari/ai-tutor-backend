a6da85aeee8aa5cee8bfff943d6a155d8481f553
1ivari <60034643+1ivari@users.noreply.github.com>
2023-04-04 18:57:44 +0300

    port change

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const { errorHandler } = require('./middleware/errorMiddleware');
 // const credentials = require('./middleware/credentials');
 const { connectDB } = require('./config/db');
 const { Pool } = require('pg');
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 // Connect to PostgreSQL
 connectDB();
